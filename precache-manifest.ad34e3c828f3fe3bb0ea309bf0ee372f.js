f05715924a101a91f493884640f974e7f39dab6f
omkar Kesarkhane <omkesarkhane@gmail.com>
2020-07-13 18:48:11 +0530

    Updates

---
+++ precache-manifest.ad34e3c828f3fe3bb0ea309bf0ee372f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a233fffa39544e6460bfb45e465344de",
+    "url": "/website/index.html"
+  },
+  {
+    "revision": "0f4154db7ac6b8690011",
+    "url": "/website/static/css/main.6034f2cf.chunk.css"
+  },
+  {
+    "revision": "5a4a4075e37ce495e80b",
+    "url": "/website/static/js/2.68cb6c2b.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/website/static/js/2.68cb6c2b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f4154db7ac6b8690011",
+    "url": "/website/static/js/main.1912ac3a.chunk.js"
+  },
+  {
+    "revision": "9c23e00f99dd7916e537",
+    "url": "/website/static/js/runtime-main.9b825fe6.js"
+  },
+  {
+    "revision": "ecd46125586db7f977f5b8ac59cc9ff0",
+    "url": "/website/static/media/leftBlue.ecd46125.png"
+  },
+  {
+    "revision": "e954a276d1f94f54477310a8ab411d84",
+    "url": "/website/static/media/rightYello.e954a276.png"
+  },
+  {
+    "revision": "b2339a1dc15a667386c5a3085c6d0c5c",
+    "url": "/website/static/media/topBlue.b2339a1d.png"
+  }
+]);
